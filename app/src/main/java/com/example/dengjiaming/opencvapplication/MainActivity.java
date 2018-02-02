a6b940d82fa9cb5d8fe059b3c3db077bbeff6083
keithActiv <974254867@qq.com>
2018-02-02 20:29:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dengjiaming/opencvapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.dengjiaming.opencvapplication;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
+
+    }
+
+    private static final int READ_EXTERNAL_STORAGE_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE
+                            , android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            , android.Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS
+                            , android.Manifest.permission.CAMERA
+                            , android.Manifest.permission.RECORD_AUDIO},
+                    READ_EXTERNAL_STORAGE_CODE);
+        }
+        // Example of a call to a native method
+        Button tv = (Button) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentFd = new Intent(MainActivity.this, OpenCVActivity.class);
+                startActivity(intentFd);
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (READ_EXTERNAL_STORAGE_CODE == requestCode) {
+            for (String string : permissions) {
+                Log.e("requestPermissions", string);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dengjiaming/opencvapplication/OpenCVActivity.java
@@ -0,0 +1,396 @@
+package com.example.dengjiaming.opencvapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.example.dengjiaming.opencvapplication.util.DetectionBasedTracker;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+
+public class OpenCVActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final int JAVA_DETECTOR = 0;
+    private static final int NATIVE_DETECTOR = 1;
+    private static final String TAG = "OpenCVActivity";
+    //图片所在文件夹
+    private static final String fileName =
+            Environment.getExternalStorageDirectory().getPath();
+    CameraBridgeViewBase mCameraView;
+
+    private Mat mGray;
+    private Mat mRgba;
+
+    private int mDetectorType = NATIVE_DETECTOR;
+    private int mAbsoluteFaceSize = 0;
+    private float mRelativeFaceSize = 0.2f;
+    private DetectionBasedTracker mNativeDetector;
+    private CascadeClassifier mJavaDetector;
+    private static final Scalar FACE_RECT_COLOR = new Scalar(0, 255, 0, 255);
+
+    private int state = 1;
+    private Size mSize0;
+    private MatOfInt mChannels[];
+    private MatOfInt mHistSize;
+    private int mHistSizeNum = 25;
+    private float[] mBuff;
+    private MatOfFloat mRanges;
+    private Point mP1;
+    private Point mP2;
+    private Scalar mColorsRGB[];
+    private Scalar mWhilte;
+    private Mat mSepiaKernel;
+
+    private Mat mTmp;
+    private Mat mTmp2;
+
+    private File mCascadeFile;
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    // OpenCV初始化加载成功，再加载本地so库
+                    System.loadLibrary("detection_based_tracker");
+
+                    try {
+                        // 加载人脸检测模式文件
+                        InputStream is = getResources()
+                                .openRawResource(R.raw.lbpcascade_frontalface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir,
+                                "lbpcascade_frontalface.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+                        byte[] buffer = new byte[4096];
+                        int byteesRead;
+                        while ((byteesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, byteesRead);
+                        }
+                        is.close();
+                        os.close();
+                        // 使用模型文件初始化人脸检测引擎
+                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        if (mJavaDetector.empty()) {
+                            Log.e(TAG, "加载cascade classifier失败");
+                            mJavaDetector = null;
+                        } else {
+                            Log.d(TAG, "Loaded cascade classifier from "
+                                    + mCascadeFile.getAbsolutePath());
+                        }
+                        mNativeDetector = new
+                                DetectionBasedTracker(mCascadeFile.getAbsolutePath(),
+                                0);
+                        cascadeDir.delete();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // 开启渲染Camera
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_opencv);
+
+        mSize0 = new Size();
+        mChannels = new MatOfInt[]{new MatOfInt(0), new MatOfInt(1), new MatOfInt(2)};
+        mBuff = new float[mHistSizeNum];
+        mHistSize = new MatOfInt(mHistSizeNum);
+        mRanges = new MatOfFloat(0f, 256f);
+        mColorsRGB = new Scalar[]{new Scalar(200, 0, 0, 255), new Scalar(0, 200, 0, 255), new Scalar(0, 0, 200, 255)};
+        mWhilte = Scalar.all(255);
+        mP1 = new Point();
+        mP2 = new Point();
+
+        // Fill sepia kernel
+        mSepiaKernel = new Mat(4, 4, CvType.CV_32F);
+        mSepiaKernel.put(0, 0, /* R */0.189f, 0.769f, 0.393f, 0f);
+        mSepiaKernel.put(1, 0, /* G */0.168f, 0.686f, 0.349f, 0f);
+        mSepiaKernel.put(2, 0, /* B */0.131f, 0.534f, 0.272f, 0f);
+        mSepiaKernel.put(3, 0, /* A */0.000f, 0.000f, 0.000f, 1f);
+
+        //读取照片
+        //Mat src = Imgcodecs.imread(fileName + File.separator + "2.jpg");
+
+        mCameraView = (CameraBridgeViewBase) findViewById(R.id.cameraView_face);
+        mCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        // 注册Camera渲染事件监听器
+        mCameraView.setCvCameraViewListener(this);
+        //设置分辨率
+        mCameraView.setMaxFrameSize(1280, 720);
+        //设置前后摄像头
+        mCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
+
+        findViewById(R.id.switchCamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //转换前后摄像头
+                if (mCameraView.switchCamera()) {
+                    showMsg("success");
+                } else {
+                    showMsg("fail");
+                }
+            }
+        });
+        findViewById(R.id.takePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //拍照,保存为png格式
+                Mat mBgr = new Mat();
+                Imgproc.cvtColor(mRgba, mBgr, Imgproc.COLOR_RGBA2BGR, 3);
+                if (Imgcodecs.imwrite(fileName + File.separator +
+                      /*  System.currentTimeMillis()*/ state + ".png", mBgr)) {
+                    state++;
+                    showMsg("success");
+                } else {
+                    showMsg("fail");
+                }
+                mBgr.release();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 静态初始化OpenCV
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "无法加载OpenCV本地库，将使用OpenCV Manager初始化");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "成功加载OpenCV本地库");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // 停止渲染Camera
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 停止渲染Camera
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        // 灰度图像
+        mGray = new Mat();
+        // R、G、B彩色图像
+        mRgba = new Mat();
+
+        mTmp = new Mat();
+
+        mTmp2 = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+        mTmp.release();
+        mTmp2.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        int cols = (int) mRgba.size().width;
+        int rows = (int) mRgba.size().height;
+
+        int left = cols / 8;
+        int top = rows / 8;
+
+        int width = cols * 3 / 4;
+        int height = rows * 3 / 4;
+
+        switch (state) {
+            case 1:
+                //旋转图片
+                Core.transpose(mRgba, mTmp);
+                Imgproc.resize(mTmp, mRgba, mRgba.size());
+                break;
+            case 2:
+                //Canny边缘检测
+                Imgproc.Canny(mGray, mTmp, 80, 100);
+                Imgproc.cvtColor(mTmp, mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+                break;
+            case 3:
+                //Hist直方图计算
+                Mat hist = new Mat();
+                int thikness = (int) (cols / (mHistSizeNum + 10) / 5);
+                if (thikness > 5) {
+                    thikness = 5;
+                }
+                int offset = (int) ((cols - (5 * mHistSizeNum + 4 * 10) * thikness) / 2);
+
+                // RGB
+                for (int c = 0; c < 3; c++) {
+                    Imgproc.calcHist(Arrays.asList(mRgba), mChannels[c], mTmp2, hist, mHistSize, mRanges);
+                    Core.normalize(hist, hist, rows / 2, 0, Core.NORM_INF);
+                    hist.get(0, 0, mBuff);
+                    for (int h = 0; h < mHistSizeNum; h++) {
+                        mP1.x = mP2.x = offset + (c * (mHistSizeNum + 10) + h) * thikness;
+                        mP1.y = rows - 1;
+                        mP2.y = mP1.y - 2 - (int) mBuff[h];
+                        Imgproc.line(mRgba, mP1, mP2, mColorsRGB[c], thikness);
+                    }
+                }
+                // Value and Hue
+                Imgproc.cvtColor(mRgba, mTmp, Imgproc.COLOR_RGB2HSV_FULL);
+                // Value
+                Imgproc.calcHist(Arrays.asList(mTmp), mChannels[2], mTmp2, hist, mHistSize, mRanges);
+                Core.normalize(hist, hist, rows / 2, 0, Core.NORM_INF);
+                hist.get(0, 0, mBuff);
+                for (int h = 0; h < mHistSizeNum; h++) {
+                    mP1.x = mP2.x = offset + (3 * (mHistSizeNum + 10) + h) * thikness;
+                    mP1.y = rows - 1;
+                    mP2.y = mP1.y - 2 - (int) mBuff[h];
+                    Imgproc.line(mRgba, mP1, mP2, mWhilte, thikness);
+                }
+                break;
+            case 4:
+                //Sobel边缘检测
+                Imgproc.Sobel(
+                        mGray.submat(top, top + height, left, left + width),
+                        mTmp, CvType.CV_8U, 1, 1);
+                Core.convertScaleAbs(mTmp, mTmp, 10, 0);
+                Imgproc.cvtColor(mTmp,
+                        mRgba.submat(top, top + height, left, left + width),
+                        Imgproc.COLOR_GRAY2BGRA, 4);
+                break;
+            case 5:
+                //SEPIA(色调变换)
+                mTmp = mRgba.submat(top, top + height, left, left + width);
+                Core.transform(mTmp, mTmp, mSepiaKernel);
+                break;
+            case 6:
+                //ZOOM放大镜
+                mTmp = mRgba.submat(0,
+                        rows / 2 - rows / 10, 0, cols / 2 - cols / 10);
+                mTmp2 = mRgba.submat(rows / 2 - 9 * rows / 100,
+                        rows / 2 + 9 * rows / 100,
+                        cols / 2 - 9 * cols / 100,
+                        cols / 2 + 9 * cols / 100);
+                Imgproc.resize(mTmp2, mTmp, mTmp.size());
+                Imgproc.rectangle(mTmp2, new Point(1, 1),
+                        new Point(mTmp2.size().width - 2, mTmp2.size().height - 2),
+                        FACE_RECT_COLOR, 2);
+                break;
+            case 7:
+                //PIXELIZE像素化
+                mTmp = mRgba.submat(top, top + height, left, left + width);
+                //放大图片
+//                float scale = 0.5f;
+//                Imgproc.resize(mTmp, mTmp2, new Size(cols * scale, rows * scale));
+
+//                scale = 1.5f;
+//                Imgproc.resize(mTmp, mTmp2, new Size(cols * scale, rows * scale));
+
+//                cols = 400;
+//                rows = 400;
+//                Imgproc.resize(mTmp, mTmp2, new Size(cols, rows));
+
+                //dsize如果等于0时，dsize = Size(round(fx * src.cols), round(fy * src.rows))
+                Imgproc.resize(mTmp, mTmp2, mSize0, 0.1, 0.1, Imgproc.INTER_NEAREST);
+                Imgproc.resize(mTmp2, mRgba.submat(0,
+                        rows / 2 - rows / 10, 0, cols / 2 - cols / 10), mRgba.submat(0,
+                        rows / 2 - rows / 10, 0, cols / 2 - cols / 10).size());
+//                Imgproc.resize(mTmp2, mTmp, mTmp.size(), 0., 0., Imgproc.INTER_NEAREST);
+                break;
+            default:
+                break;
+        }
+        // 设置脸部大小
+        if (mAbsoluteFaceSize == 0) {
+            height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+            mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
+        }
+
+        // 获取检测到的脸部数据
+        MatOfRect faces = new MatOfRect();
+        if (mDetectorType == JAVA_DETECTOR) {
+            if (mJavaDetector != null) {
+                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2,
+                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+
+            }
+        } else if (mDetectorType == NATIVE_DETECTOR) {
+            if (mNativeDetector != null) {
+//                Log.v(TAG, "start");
+                mNativeDetector.detect(mGray, faces);
+//                Log.v(TAG, "end");
+            }
+        } else {
+            Log.e(TAG, "Detection method is not selected!");
+        }
+
+        // 绘制检测框
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++) {
+            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+        }
+        if (faces.toArray().length > 0) {
+            Log.d(TAG, "共检测到 " + faces.toArray().length + " 张脸");
+        }
+        return mRgba;
+    }
+
+    private void showMsg(String msg) {
+        Toast.makeText(OpenCVActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
